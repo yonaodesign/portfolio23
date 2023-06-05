d76e2e980fcd921f9a289e9e77caff1c348566d4
yonaodesign <52710606+yonaodesign@users.noreply.github.com>
2023-06-05 21:48:00 +0200

    removedTheming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,7 @@
-import '@/styles/globals.css'
-import React from 'react'
-import type { AppProps } from 'next/app'
-import { useState, createContext, useContext } from 'react'
-
-import { ThemeProvider } from "styled-components"
-import { lightTheme, darkTheme, GlobalStyles } from "../ThemeConfig"
-
-const themesMap = {
-  lightTheme,
-  darkTheme
-}
-export const ThemePreferenceContext = React.createContext()
+import "@/styles/globals.css";
+import React from "react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [currentTheme, setCurrentTheme] = useState('light')
-
-  const theme = { ...GlobalStyles, colors: themesMap[currentTheme] }
-
-
-
-  return (
-    <ThemePreferenceContext.Provider value={{ currentTheme, setCurrentTheme }}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ThemePreferenceContext.Provider>
-  )
+  return <Component {...pageProps} />;
 }
-
-
-
